ec97ebed7646cb995028df1a7c495f2df818e26f
Saiful Islam <codersaiful@gmail.com>
2021-11-03 16:55:21 +0600

    aro onek kaj kora hoeyche. samner aro kaj kora hobe

--- app/shortcode.php
+++ app/shortcode.php
@@ -9,19 +9,28 @@ class Shortcode{
     public static $post_type = 'post';
     public static $atts = array();
 
+
+    public static function attr_to_arr($atts_attr = 'cats'){
+        
+        $atts = self::$atts;
+        $catstr = $atts[$atts_attr];
+        if( ! is_string( $catstr ) ) return;
+
+        $catstr = rtrim($catstr,',');
+        $cats = explode(',',$catstr);
+
+        $cats = array_filter($cats);
+        $cats = array_map(function($item){
+            return (int) $item;
+        },$cats);
+        return $cats;
+    }
+
     public static function init( $atts ){
         
         self::$atts = $atts;
         if( isset( $atts['cats'] ) && ! empty( $atts['cats'] ) ){
-            $catstr = $atts['cats'];
-            $catstr = rtrim($catstr,',');
-            $cats = explode(',',$catstr);
-
-            $cats = array_filter($cats);
-            $cats = array_map(function($item){
-                return (int) $item;
-            },$cats);
-            self::$cats = $cats;
+            self::$cats = self::attr_to_arr('cats');
         }
 
         //Generate for all Cate
@@ -29,10 +38,22 @@ class Shortcode{
             self::$cats = self::get_categories();
         }
 
+        //self::$cats = self::get_cat_modified_ids();
+
         if( ! is_array( self::$cats ) || count( self::$cats ) < 1 ) return;
 
         
         return self::generate_html();
+    }
+
+    public static function get_cat_modified_ids(){
+        $modified_cats = self::$cats;
+
+
+
+
+        return apply_filters( 'wppcd_cats_list_arr', $modified_cats , self::$atts );
+
     }
 
     public static function get_categories(){
@@ -43,7 +64,7 @@ class Shortcode{
         );
         $o_cats = array();
         foreach( $categories as $category ){
-            $o_cats[] = $category->term_id;
+            $o_cats[$category->term_id] = $category->term_id;
         }
         return $o_cats;
     }
@@ -73,10 +94,11 @@ class Shortcode{
 
     public static function taxonomy_markup( $cat_id = false ){
         if( ! $cat_id ) return;
+        if(empty( self::get_taxonomy_name( $cat_id ) )) echo "The Taxonomy ID:$cat_id not found";
         ?>
         <div class="each-taxonomy-item-wrapper">
             <div class="each-item-inside">
-                <h3 class="item-heading"><?php echo esc_html( self::get_taxonomy_name( $cat_id ) ) ?></h3>
+                <h3 class="item-heading <?php echo esc_attr( $cat_id ); ?>"><?php echo esc_html( self::get_taxonomy_name( $cat_id ) ) ?></h3>
                 <?php
                 self::the_post_list_markup( $cat_id );
                 ?>
@@ -116,7 +138,7 @@ class Shortcode{
         
         if( $query->have_posts() ):
             ?>
-            <ul class="item-list item-list-id-<?php echo $cat_id; ?>">
+            <ul class="item-list item-list-id-<?php echo esc_attr( $cat_id ); ?>">
             <?php 
             while( $query->have_posts() ): $query->the_post();
 
