d2965fc456179f05b19a276973fca7f70035b543
Saiful Islam <codersaiful@gmail.com>
2025-05-22 18:36:49 +0600

    removed wc frompost classified plugin

--- init.php
+++ init.php
@@ -10,8 +10,6 @@
  * Version: 1.2.0
  * Requires at least:    4.0.0
  * Tested up to:         6.8
- * WC requires at least: 4.0
- * WC tested up to:      9.8.5
  * 
  * *********************
  * SHORTCODE EXAMPLE: 
